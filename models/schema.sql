b17d76bd6fd149448f3246016dbe3bba04bde2d0
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-07-30 20:59:23 -0500

    fixed api request code

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,18 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS upsteam_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE upsteam_db;
+USE upsteam_db;
+
+CREATE TABLE `upstream` (
+ `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+ `Title` VARCHAR( 200 ) NOT NULL,
+ `Info` TEXT NOT NULL,
+ `Actors` TEXT NOT NULL,
+ `Cover_Photo_url` TEXT NOT NULL,
+ `Streaming_Services` VARCHAR( 500 ) NOT NULL,
+ `Release_Date` DATETIME NOT NULL,
+ 'Review' TEXT,
+ 'Watched' TINYINT(1)
+
+ PRIMARY KEY ( `id` )
+);
