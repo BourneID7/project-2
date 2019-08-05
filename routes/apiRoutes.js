812633c23f23c7d6222bdf17c82a3bb97b1870f9
Jason Bourne <m.bourne@wi.rr.com>
2019-08-04 19:23:40 -0500

    comment out unused authentication

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -177,81 +177,81 @@ module.exports = function(app) {
   });
 
     // Get all users
-    app.get("/login", function(req, res) {
-      db.User.findAll({}).then(function(dbUsers) {
-        console.log("username: ", req.username);
-        console.log("authenticated: ", req.isAuthenticated());
-        res.json(dbUsers);
-      });
-    });
+    // app.get("/login", function(req, res) {
+    //   db.User.findAll({}).then(function(dbUsers) {
+    //     console.log("username: ", req.username);
+    //     console.log("authenticated: ", req.isAuthenticated());
+    //     res.json(dbUsers);
+    //   });
+    // });
   
 
   // post route to add new user
-  app.post("/register", function(req, res) {
-    console.log(req.body);
+  // app.post("/register", function(req, res) {
+  //   console.log(req.body);
 
-    req.checkBody(req.body.username, "Username cannot be empty").notEmpty();
-    req.checkBody(req.body.password, "Password cannot be empty").notEmpty();
-    req.checkBody(req.body.password, "Password length must between 8 and 20 characters").len(8, 100);
-    req.checkBody(req.body.passwordMatch, "Password length must between 8 and 20 characters").len(8, 100);
-    req.checkBody(req.body.passwordMatch, "Passwords do not match. Try again.").equals(req.body.password);
+  //   req.checkBody(req.body.username, "Username cannot be empty").notEmpty();
+  //   req.checkBody(req.body.password, "Password cannot be empty").notEmpty();
+  //   req.checkBody(req.body.password, "Password length must between 8 and 20 characters").len(8, 100);
+  //   req.checkBody(req.body.passwordMatch, "Password length must between 8 and 20 characters").len(8, 100);
+  //   req.checkBody(req.body.passwordMatch, "Passwords do not match. Try again.").equals(req.body.password);
 
-    var errors = req.validationErrors();
+  //   var errors = req.validationErrors();
 
-    if (errors) {
-      console.log("errors: ", errors);
-    };
+  //   if (errors) {
+  //     console.log("errors: ", errors);
+  //   };
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-      db.User.create({
-        username: req.body.username,
-        password: hash,
-        createdAt: new Date()
-      }).then(function(dbUser) {
-        res.json(dbUser);
-        console.log("string DBUSER: ", dbUser);
+  //   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  //     // Store hash in your password DB.
+  //     db.User.create({
+  //       username: req.body.username,
+  //       password: hash,
+  //       createdAt: new Date()
+  //     }).then(function(dbUser) {
+  //       res.json(dbUser);
+  //       console.log("string DBUSER: ", dbUser);
 
-        db.User.findOne({
-          // where: {
-          //   username: dbUser.dataValues.username
-          // }
-          order: [ [ "createdAt", "DESC" ]]
-        }).then(function(results) {
-          // if (err) throw err;
-          console.log("is this being read 2?");
-          console.log(results);
-          var user_id = results[0];
+  //       db.User.findOne({
+  //         // where: {
+  //         //   username: dbUser.dataValues.username
+  //         // }
+  //         order: [ [ "createdAt", "DESC" ]]
+  //       }).then(function(results) {
+  //         // if (err) throw err;
+  //         console.log("is this being read 2?");
+  //         console.log(results);
+  //         var user_id = results[0];
   
-          req.login(user_id, function(err) {
-            if (err) throw err;
-            console.log("username: ", req.username);
-            console.log("authenticated: ", req.isAuthenticated());
+  //         req.login(user_id, function(err) {
+  //           if (err) throw err;
+  //           console.log("username: ", req.username);
+  //           console.log("authenticated: ", req.isAuthenticated());
     
-            // createCookie();
-            res.redirect("/watch");
-          });
-        }); 
+  //           // createCookie();
+  //           res.redirect("/watch");
+  //         });
+  //       }); 
   
-      })
-      .catch(function(err) {
-        // print the error details
-        console.log(err, req.body.username);
-      });
-    });
-  });
+  //     })
+  //     .catch(function(err) {
+  //       // print the error details
+  //       console.log(err, req.body.username);
+  //     });
+  //   });
+  // });
 
-  passport.serializeUser(function(user_id, done) {
-    done(null, user_id);
-  });
+  // passport.serializeUser(function(user_id, done) {
+  //   done(null, user_id);
+  // });
    
-  passport.deserializeUser(function(user_id, done) {
-    done(null, user_id);
-  });
+  // passport.deserializeUser(function(user_id, done) {
+  //   done(null, user_id);
+  // });
 
-  app.post("/login", 
-    passport.authenticate("local", { failureRedirect: "/watch" }),
-    function(req, res) {
-      res.redirect("/");
-  });
+  // app.post("/login", 
+  //   passport.authenticate("local", { failureRedirect: "/watch" }),
+  //   function(req, res) {
+  //     res.redirect("/");
+  // });
 };
